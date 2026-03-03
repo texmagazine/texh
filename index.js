8253527e580283b520dd8534074e88b67d670a21
texmagazine <261240455+texmagazine@users.noreply.github.com>
2026-03-03 23:29:28 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,80 @@
+/* VIDÉOS */
+const goatVideo = document.querySelector(".goat-video");
+
+const startVideo = "chevre/start.mp4";
+const successVideo = "chevre/success.mp4";
+
+const errorVideos = [
+  "chevre/error01.mp4",
+  "chevre/error02.mp4",
+  "chevre/error03.mp4",
+  "chevre/error04.mp4",
+  "chevre/error05.mp4",
+  "chevre/error06.mp4"
+];
+
+function checkPassword() {
+  const goodPassword = "sos";   // ton mot de passe
+  const input = document.getElementById("password").value;
+
+  const bubble = document.querySelector(".goat-speech");
+
+  const successMessages = [
+    "Mot de passe correct !",
+    "Bravo !",
+    "C’est le bon mot de passe !",
+    "Bien joué !",
+    "Mot de passe correct !",
+    "Accès autorisé !"
+  ];
+
+  const errorMessages = [
+    "Mauvais mot de passe !",
+    "Beeh, ce n'est pas le bon mot de passe",
+    "Non, ce n’est pas ça.",
+    "Mauvais mot de passe !",
+    "Non, ce n’est pas ça.",
+    "Essaie encore !"
+  ];
+
+  /* MOT DE PASSE CORRECT */
+  if (input === goodPassword) {
+
+
+    goatVideo.muted = false;   // 🔊 autorise le son
+    goatVideo.volume = 1;      // 🔊 volume normal
+
+    goatVideo.src = successVideo;
+    goatVideo.play();
+
+
+
+
+
+    bubble.textContent = successMessages[Math.floor(Math.random() * successMessages.length)];
+    bubble.style.display = "block";
+
+    goatVideo.onended = () => {
+      window.location.href = "accueil.html";
+    };
+
+    return false;
+  }
+
+
+/* MOT DE PASSE INCORRECT */
+bubble.textContent = errorMessages[Math.floor(Math.random() * errorMessages.length)];
+bubble.style.display = "block";
+
+goatVideo.muted = false;   // 🔊 active le son
+goatVideo.volume = 1;      // 🔊 volume normal
+
+const randomErrorVideo = errorVideos[Math.floor(Math.random() * errorVideos.length)];
+goatVideo.src = randomErrorVideo;
+goatVideo.play();
+
+
+
+
+  return false;
+}
